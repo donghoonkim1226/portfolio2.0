022a9e06c3a443d57946cff5dedb4f9af3f4a738
dong hoon kim <donghoonkim1226@gmail.com>
2016-02-10 15:48:13 -0500

    fixed some codes to get the links to work in side. Also added a 404 page.

--- server.js
+++ server.js
@@ -3,58 +3,34 @@ var bodyParser = require("body-parser");
 var app = express();
 var session = require("express-session");
 var middleware = require("./middleware.js");
-
 var PORT = process.env.PORT || 8080;
-
-app.use("/static", express.static("public"));
-
-app.get("/", function(req, res) {
-  res.sendFile(process.cwd() + "views/index.html");
-});
-
-app.get("/rps", function(req, res) {
-  res.sendFile(process.cwd() + "views/rps.html");
-});
-
-app.get("/clicky", function(req, res) {
-  res.sendFile(process.cwd() + "views/clicky.html");
-});
-
-app.get("/signin", function(req, res) {
-  res.sendFile(process.cwd() + "/views/signin.html");
-});
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(session({
   secret: "jlskdjfsd09fsd90f8sdfsfdfsdfs",
-  cookie: {},
+  cookie: {
+    maxAge: 60000
+  },
   saveUninitialized: true,
   resave: false
 }));
 
-app.get("/account", middleware.isAuthenticated, function(req, res) {
-  res.sendFile(process.cwd() + "/views/account.html");
+app.use("/static", express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/signin", function(req, res){
-  if(req.body.email === "a@a.com" && req.body.password === "a") {
-    if (StayLoggedIn = "on"){
-      //sets an expires date far in the future so user will stay logged on virtually forever (Dec 31 9999)
-      req.session.cookie.expires = new Date(253402300000000);
-    }else{
-      //set cookie to expire after 10 minutes
-      req.session.cookie.maxAge = 60000;
-    }
-    req.session.isAuthenticated = true;
-    res.redirect("/account");
-  }else{
-    req.session.isAuthenticated = false;
-    res.redirect("/signin");
-  }
-})
+app.get("/rps", function(req, res) {
+  res.sendFile(process.cwd() + "/views/rps.html");
+});
 
-app.get("/:anythingelse", function(req, res) {
+app.get("/clicky", function(req, res) {
+  res.sendFile(process.cwd() + "/views/clicky.html");
+});
+
+app.get("/:default", function(req, res) {
   res.status(404);
   res.sendFile(process.cwd() + "/views/404.html");
 });
